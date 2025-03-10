d2d3204a21ba9edc904dfbd2a51c2fe66e5caac8
nqd1 <nqd001205@gmail.com>
2025-03-10 10:00:49 +0700

    fixed bugs in w5h3

--- w5/w5h3.asm
+++ w5/w5h3.asm
@@ -10,13 +10,15 @@ get_string:
 	la a1, string
 	li a2, 50
 	ecall 
+	li a3, 0xa #value of newline in ascii
 get_length:
 	la a0, string # a0 = address(string[0])
 	li t0, 0 # t0 = i = 0
 	check_char:
 	add t1, a0, t0 # t1 = a0 + t0 = address(string[0]+i) 
-	lb t2, 1(t1) # t2 = string[i]
-	beq t2, zero, end_of_str # Is null char? 
+	lb t2, 0(t1) # t2 = string[i]
+	beq t2, zero, end_of_str # Is null char?
+	beq t2, a3, end_of_str # Is newline char?
 	addi t0, t0, 1 # t0 = t0 + 1 -> i = i + 1
 	j check_char
 end_of_str:
